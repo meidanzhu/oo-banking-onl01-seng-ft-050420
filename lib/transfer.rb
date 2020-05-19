3d966305f244f7a68880577fb061161c422d0a54
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-19 21:06:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,10 +22,10 @@ class Transfer
     # if @amount > @sender.balance
     #   @status = "rejected"
     #   return "Transaction rejected. Please check your account balance."
-    # 
+    #
     # end
   end
-  
+
   def reverse_transfer
 
   end
