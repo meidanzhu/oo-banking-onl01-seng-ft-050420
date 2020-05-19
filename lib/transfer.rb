6b28d028e4631eecf3b4d9fe6f8a3e5420dbb7b4
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-19 21:01:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction #sender needs to have enough money to transder to the receiver
-    if @amount > sender.balance
+    if @amount > @sender.balance
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
