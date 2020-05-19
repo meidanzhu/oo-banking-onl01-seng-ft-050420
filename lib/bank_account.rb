41484d5c7a3519d5b1f02509cb9fae2e66c4fab8
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-19 19:52:25 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,10 @@
 class BankAccount
     attr_accessor :name, :balance, :status
 
-  def initialize(name, balance=1000, status="open")
+  def initialize(name, balance, status)
     @name = name
-    @balance = balance
-    @status = status
+    @balance = 1000
+    @status = "open"
   end
 
 end
