bca2a4ef05de8e2713518999e1a04e35606f5c76
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-19 21:41:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,7 @@ class Transfer
     if @amount > @sender.balance
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    else
-      @sender.balance -= @amount
+    elsif @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     end
